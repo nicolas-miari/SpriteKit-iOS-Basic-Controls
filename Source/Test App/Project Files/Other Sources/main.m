145fa0ba9174eafd5908fd8a5d0871ba27ff8024
Nicolás Miari <nicolasmiari@gmail.com>
2014-06-09 19:02:58 +0900

    - Created test app workspace/project.

---
+++ Source/Test App/Project Files/Other Sources/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SpriteKit iOS Basic Controls
+//
+//  Created by Nicolas Miari on 6/9/14.
+//  Copyright (c) 2014 Nicolás Miari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
