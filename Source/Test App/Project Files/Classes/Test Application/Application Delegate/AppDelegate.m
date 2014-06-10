817043abb2cb2d18e883bd26674ae272c3454310
Nicolás Miari <nicolasmiari@gmail.com>
2014-06-10 12:21:28 +0900

    - More code formatting.

--- Source/Test App/Project Files/Classes/Test Application/Application Delegate/AppDelegate.m
+++ Source/Test App/Project Files/Classes/Test Application/Application Delegate/AppDelegate.m
@@ -39,12 +39,13 @@
 - (BOOL) application:(UIApplication*) application didFinishLaunchingWithOptions:(NSDictionary*) launchOptions
 {
     // Override point for customization after application launch.
+    
     return YES;
 }
 
 // .............................................................................
 
-- (void)applicationWillResignActive:(UIApplication *)application
+- (void) applicationWillResignActive:(UIApplication*) application
 {
     /* Sent when the application is about to move from active to inactive state. 
      This can occur for certain types of temporary interruptions (such as an 
@@ -57,7 +58,7 @@
 
 // .............................................................................
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
+- (void) applicationDidEnterBackground:(UIApplication*) application
 {
     /* Use this method to release shared resources, save user data, invalidate 
      timers, and store enough application state information to restore your 
@@ -69,7 +70,7 @@
 
 // .............................................................................
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
+- (void) applicationWillEnterForeground:(UIApplication*) application
 {
     /* Called as part of the transition from the background to the inactive 
      state; here you can undo many of the changes made on entering the 
@@ -79,7 +80,7 @@
 
 // .............................................................................
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
+- (void) applicationDidBecomeActive:(UIApplication*) application
 {
     /* Restart any tasks that were paused (or not yet started) while the 
      application was inactive. If the application was previously in the 
@@ -89,7 +90,7 @@
 
 // .............................................................................
 
-- (void)applicationWillTerminate:(UIApplication *)application
+- (void) applicationWillTerminate:(UIApplication*) application
 {
     /* Called when the application is about to terminate. Save data if 
      appropriate. See also applicationDidEnterBackground:.
